695a1c4305143e91b6900732063b50c61fbcf6a9
Ben Lovell <benjamin.lovell@gmail.com>
2018-07-30 15:04:31 +0100

    Add further booking request validation rules

--- app/models/booking_request.rb
+++ app/models/booking_request.rb
@@ -2,6 +2,15 @@ class BookingRequest < ApplicationRecord
   belongs_to :location, optional: true
 
   validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, email: true
+  validates :phone, presence: true
+  validates :memorable_word, presence: true
+  validates :accessibility_needs, inclusion: { in: [true, false] }
+  validates :defined_contribution_pot_confirmed, inclusion: { in: [true, false] }
+  validates :additional_info, length: { maximum: 320 }, allow_blank: true
+  validates :where_you_heard, presence: true
+  validates :gdpr_consent, inclusion: { in: ['yes', 'no', ''] }
 
   def face_to_face?
     location_id.present?
